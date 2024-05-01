018c7f389d72a2bb6f306c3ea13f1e153f582f78
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-05-01 19:39:23 +0300

    Initial commit added template for API controller.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Activity> Activities { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
