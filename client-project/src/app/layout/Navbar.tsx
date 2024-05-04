0466d5f08285c9ee5549315a2ed115b81c685d18
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-05-05 00:03:05 +0300

    Added CRUD operations in client-side of the application. Database functionality is not inplemented yet between back-and and fron-end.

---
+++ client-project/src/app/layout/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+interface Props {
+  openForm: () => void;
+}
+
+export default function Navbar({ openForm }: Props) {
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <Menu.Item>
+          <img
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginRight: '10px' }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name='Activities' />
+        <Menu.Item>
+          <Button positive content='Create Activity' onClick={openForm} />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+}
